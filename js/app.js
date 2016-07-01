cc4f59af482e5199d8c9db3440622030c627b99f
Ryca77 <ryan.carter77@gmail.com>
2016-07-01 14:43:12 +0100

    ja/app.js

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ function getLocation() {
 		console.log(latitude);
 		console.log(longitude);
 		var mapImage = new Image();
-		mapImage.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=15&size=300x300&sensor=false";
+		mapImage.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=15&size=300x300&sensor=false&key=AIzaSyDf2HC-XcAoYMwtj02odxmqeBfTvYqGNdQ";
 	
 		bikeMap.appendChild(mapImage);
 	};
