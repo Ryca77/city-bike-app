063022aa1de83549253e1d42907b1b5837214398
Ryca77 <ryan.carter77@gmail.com>
2016-07-26 20:57:12 +0100

    package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,16 @@ var webpack = require('webpack');
 
 var packageData = require('./package.json');
 
+var minify = process.argv.indexOf('--minify') != -1;
+
 var filename = [packageData.name, packageData.version, 'js'];
+
+var plugins = [];
+
+if (minify) {
+    filename.splice(filename.length - 1, 0, 'min');
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
 
 module.exports = {
     entry: path.resolve(__dirname, packageData.main),
@@ -24,5 +33,6 @@ module.exports = {
           }
         }
       ]
-    }
+    },
+    plugins: plugins
 }
